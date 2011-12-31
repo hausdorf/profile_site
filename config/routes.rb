7c7b23e496110e3a4e287edbba1e68a48cd09887
Alex Clemmer <clemmer.alexander@gmail.com>
2011-12-30 18:19:34 -0700

    Update routes once again

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Home::Application.routes.draw do
 
   match "about" => "about#index"
-  match "learning" => "learning#list"
+  match "learning/list" => "learning#list"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
